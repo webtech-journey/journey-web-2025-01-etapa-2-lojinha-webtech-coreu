17e4a28af343bd52b6238140424e1c1d361572b3
joaovitoralvarenga <ggquennbr@gmail.com>
2025-11-11 16:08:50 -0300

    feat: added tailwind colors

--- webtechstore/app/page.tsx
+++ webtechstore/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="max-w-md text-lg leading-8 text-finish-button dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
